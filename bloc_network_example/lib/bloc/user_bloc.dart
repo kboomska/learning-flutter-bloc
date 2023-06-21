66808cf09543453d5dae90b7e75463ff8e5562b7
Aleksey Kuzmin <kboomska@gmail.com>
2023-06-21 14:39:12 +0300

    Chapter 12: Flutter BLoC. Network requests (creating UserBloc)

---
+++ bloc_network_example/lib/bloc/user_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bloc_network_example/services/user_repository.dart';
+import 'package:bloc_network_example/bloc/user_event.dart';
+import 'package:bloc_network_example/bloc/user_state.dart';
+import 'package:bloc_network_example/model/user.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository userRepository;
+
+  UserBloc({
+    required this.userRepository,
+  }) : super(UserEmptyState()) {
+    on<UserLoadEvent>(
+      (event, emit) async {
+        emit(UserLoadingState());
+        try {
+          final List<User> loadedUserList = await userRepository.getAllUsers();
+          emit(UserLoadedState(loadedUser: loadedUserList));
+        } catch (_) {
+          emit(UserErrorState());
+        }
+      },
+    );
+    on<UserClearEvent>(
+      (event, emit) {
+        emit(UserEmptyState());
+      },
+    );
+  }
+}
