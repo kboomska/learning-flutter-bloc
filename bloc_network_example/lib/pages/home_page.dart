97e75de928b5400f6c7b5b3fccfcba00852ee742
Aleksey Kuzmin <kboomska@gmail.com>
2023-06-22 10:42:09 +0300

    Chapter 12: Flutter Cubit. Network requests (implementing cubit)

--- bloc_network_example/lib/pages/home_page.dart
+++ bloc_network_example/lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:bloc_network_example/services/user_repository.dart';
 import 'package:bloc_network_example/widgets/action_buttons.dart';
+import 'package:bloc_network_example/cubit/internet_cubit.dart';
 import 'package:bloc_network_example/widgets/user_list.dart';
 import 'package:bloc_network_example/bloc/user_event.dart';
 import 'package:bloc_network_example/bloc/user_bloc.dart';
@@ -15,13 +16,26 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider(
       create: (context) => UserRepository(),
-      child: BlocProvider(
-        create: (context) => UserBloc(
-          userRepository: context.read<UserRepository>(),
-        )..add(UserLoadEvent()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => UserBloc(
+              userRepository: context.read<UserRepository>(),
+            )..add(UserLoadEvent()),
+          ),
+          BlocProvider(
+            create: (context) => ConnectionCubit(),
+          )
+        ],
         child: Scaffold(
           appBar: AppBar(
-            title: const Text('User List'),
+            title: BlocBuilder<ConnectionCubit, MyConnectionState>(
+              builder: (context, state) => state.connected
+                  ? const Text('User List (в сети)')
+                  : const Text(
+                      'Нет соединения с интернетом!',
+                    ),
+            ),
             centerTitle: true,
           ),
           backgroundColor: Colors.white,
